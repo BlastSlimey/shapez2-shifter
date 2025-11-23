8e13cadc09b48d1fd2cf333f8015c0732299d50d
Raphdf201 <raphdf201@hotmail.com>
2025-11-23 18:33:50 -0500

    Default to Wire BuildingSignalIOType in WireConnectorConfig

--- ShapezShifter/src/ShapezShifter.Flow/AtomicExtender/Building/BuildingConnectorConfigs.cs
+++ ShapezShifter/src/ShapezShifter.Flow/AtomicExtender/Building/BuildingConnectorConfigs.cs
@@ -38,30 +38,30 @@ namespace ShapezShifter.Flow.Atomic
         public readonly BuildingSignalIOType IoType;
 
         private WireConnectorConfig(TileDirection direction,
-            BuildingSignalIOType ioType = BuildingSignalIOType.Building)
+            BuildingSignalIOType ioType = BuildingSignalIOType.Wire)
         {
             Direction = direction;
             IoType = ioType;
         }
 
-        public static WireConnectorConfig DefaultInput(BuildingSignalIOType ioType = BuildingSignalIOType.Building)
+        public static WireConnectorConfig DefaultInput(BuildingSignalIOType ioType = BuildingSignalIOType.Wire)
         {
             return new WireConnectorConfig(TileDirection.West, ioType);
         }
 
-        public static WireConnectorConfig DefaultOutput(BuildingSignalIOType ioType = BuildingSignalIOType.Building)
+        public static WireConnectorConfig DefaultOutput(BuildingSignalIOType ioType = BuildingSignalIOType.Wire)
         {
             return new WireConnectorConfig(TileDirection.East, ioType);
         }
 
         public static WireConnectorConfig CustomInput(TileDirection direction,
-            BuildingSignalIOType ioType = BuildingSignalIOType.Building)
+            BuildingSignalIOType ioType = BuildingSignalIOType.Wire)
         {
             return new WireConnectorConfig(direction, ioType);
         }
 
         public static WireConnectorConfig CustomOutput(TileDirection direction,
-            BuildingSignalIOType ioType = BuildingSignalIOType.Building)
+            BuildingSignalIOType ioType = BuildingSignalIOType.Wire)
         {
             return new WireConnectorConfig(direction, ioType);
         }
